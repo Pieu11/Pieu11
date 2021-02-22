3a586fda08cc107fe3782f661e83f8cce6b987c7
Valentina Arenas Lozano <Valentinaarenas2@gmail.com>
2021-02-21 21:07:09 -0500

    Se organiza la parte de contactos, html y css, desde los archivos pug y scss, queda buscar los iconos para el contacto y organizar el margen de los project para que se puedan agregar 4 por página

--- js/script.js
+++ js/script.js
@@ -1,3 +1,60 @@
-var home = document.getElementsByClassName("home")
-var designs = document.getElementsByClassName("designs")
-var webPages = document.getElementsByClassName("webPages")
+//**************************************** SECTIONS
+home = document.querySelector(".home")
+designs = document.querySelector(".designs")
+webPages = document.querySelector(".webPages")
+apps = document.querySelector(".apps")
+animations = document.querySelector(".animations")
+contacts = document.querySelector(".contacts")
+
+//**************************************** SECTIONS LINKS
+linkHome = document.querySelector(".homeMenu")
+linkDesigns = document.querySelector(".designsMenu")
+linkWebPages = document.querySelector(".webPagesMenu")
+linkApps = document.querySelector(".appsMenu")
+linkAnimations = document.querySelector(".animationsMenu")
+linkContacts = document.querySelector(".contactsMenu")
+
+//**************************************** EVENT FOR LINKS
+linkHome.addEventListener('click',deployHome)
+linkDesigns.addEventListener('click',deployDesign)
+linkWebPages.addEventListener('click',deployWebPages)
+linkApps.addEventListener('click',deployApps)
+linkAnimations.addEventListener('click',deployAnimations)
+linkContacts.addEventListener('click',deployContacts)
+
+function deployHome(){
+    deploy(home)
+}
+function deployDesign(){
+    deploy(designs)
+}
+function deployWebPages(){
+    deploy(webPages)
+}
+function deployApps(){
+    deploy(apps)
+}
+function deployAnimations(){
+    deploy(animations)
+}
+function deployContacts(){
+    deploy(contacts)
+}
+
+function deploy(section){
+    if(section.classList.contains('deploy')){
+        console.log('good');
+    }else{
+        removeDeploy()
+        section.classList.add('deploy')
+    }
+}
+
+function removeDeploy(){
+    home.classList.remove('deploy')
+    designs.classList.remove('deploy')
+    webPages.classList.remove('deploy')
+    apps.classList.remove('deploy')
+    animations.classList.remove('deploy')
+    contacts.classList.remove('deploy')
+}
